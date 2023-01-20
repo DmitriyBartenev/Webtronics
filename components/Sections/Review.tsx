4c9e96d350a42dff154cac8b52219ad0ece21e62
DmitriyBartenev <bartenevdmitriy@bk.ru>
2023-01-20 18:41:01 +0400

    added slider

--- components/Sections/Review.tsx
+++ components/Sections/Review.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Image from 'next/image';
 
 import styles from '../../styles/Review.module.scss';
@@ -7,8 +7,23 @@ import leftArrow from '../../public/leftArrow.svg';
 import rightArrow from '../../public/rightArrow.svg';
 import Photo1 from '../../public/photo1.png';
 import Photo2 from '../../public/photo2.png';
+import Photo3 from '../../public/photo3.png';
 
 const Review: React.FC = () => {
+
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    const images = [
+        '/bigPhoto1.png',
+        '/bigPhoto2.png',
+        '/bigPhoto3.png'
+    ]
+    
+    const smallImages = [
+        '/photo1.png',
+        '/photo2.png',
+        '/photo3.png'
+    ]
     
     return(
         <section className={styles.review}>
@@ -20,15 +35,22 @@ const Review: React.FC = () => {
                 </div>
                 <div className={styles.slider}>
                     <div className={styles.slider__header}>
-                        <p>1/3</p>
+                        <p>{currentIndex+1}/3</p>
                         <div className={styles.arrow_container}>
-                            <Image src={leftArrow} alt='left arrow'/>
-                            <Image src={rightArrow} alt='right arrow'/>
+                            <button 
+                                className={styles.left_arrow}
+                                disabled={currentIndex === 0}
+                                onClick={() => setCurrentIndex(currentIndex - 1)}/>
+                            <button 
+                                className={styles.right_arrow}
+                                disabled={currentIndex === 2}
+                                onClick={() => setCurrentIndex(currentIndex + 1)}/>
                         </div>
                     </div>
                     <div className={styles.photos}>
-                        <Image src={Photo1} alt='person'/>
-                        <Image src={Photo2} alt='person'/>
+                        <Image src={images[currentIndex]} alt='person' width={392} height={422}/>
+                        <Image src={currentIndex === 1 ? smallImages[2] : '' || currentIndex === 2 ? smallImages[0] : '' || currentIndex === 0 ? smallImages[1] : ''} alt='person' width={184} height={422}/>
+                        <Image src={currentIndex === 1 ? smallImages[0] : '' || currentIndex === 2 ? smallImages[1] : '' || currentIndex === 0 ? smallImages[2] : ''} alt='person' width={184} height={422}/>
                     </div>
                 </div>
             </div>
